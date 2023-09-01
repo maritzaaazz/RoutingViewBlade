1e28f09aec798decbacd2d040687af1cb348743a
maritzaaazz <maritzaangelinaazzahra@mail.ugm.ac.id>
2023-09-01 07:30:03 +0700

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <header>
+        <h3>Header</h3>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+    <footer>
+        <h5>Footer</h5>
+    </footer>
+</body>
+</html>
+
+
